587320b1f785d045bab9b27c61b3ed38446565db
winnieli1129 <leechangwook0621@gmail.com>
2020-03-01 00:53:58 -0600

    update reference

--- src/components/reference.jsx
+++ src/components/reference.jsx
@@ -1,26 +1,29 @@
 import React from 'react'
 function Reference() {
     return(
-		<div id = "referenceWrapper">
-			<h1>References</h1>
-			<p>Hoffman, R. A. (1983). Grade inflation and student evaluations of college courses. Educational
-			and Psychological Research, 3, 51–160. https://doi.org/10.1023/A:101557981</p>
-			<p>Hoffman, R. A. (1983). Grade inflation and student evaluations of college courses. Educational
-			and Psychological Research, 3, 51–160. https://doi.org/10.1023/A:101557981</p>
-			<p>Hoffman, R. A. (1983). Grade inflation and student evaluations of college courses. Educational
-			and Psychological Research, 3, 51–160. https://doi.org/10.1023/A:101557981</p>
-			<p>Hoffman, R. A. (1983). Grade inflation and student evaluations of college courses. Educational
-			and Psychological Research, 3, 51–160. https://doi.org/10.1023/A:101557981</p>
-			<p>Hoffman, R. A. (1983). Grade inflation and student evaluations of college courses. Educational
-			and Psychological Research, 3, 51–160. https://doi.org/10.1023/A:101557981</p>
-			<p>Hoffman, R. A. (1983). Grade inflation and student evaluations of college courses. Educational
-			and Psychological Research, 3, 51–160. https://doi.org/10.1023/A:101557981</p>
-			<p>Hoffman, R. A. (1983). Grade inflation and student evaluations of college courses. Educational
-			and Psychological Research, 3, 51–160. https://doi.org/10.1023/A:101557981</p>
-			<p>Hoffman, R. A. (1983). Grade inflation and student evaluations of college courses. Educational
-			and Psychological Research, 3, 51–160. https://doi.org/10.1023/A:101557981</p>
-			<p>Hoffman, R. A. (1983). Grade inflation and student evaluations of college courses. Educational
-			and Psychological Research, 3, 51–160. https://doi.org/10.1023/A:101557981</p>
+	    <div>
+			<div id = "home"><a href = "#">home</a></div>
+			<div id = "referenceWrapper">
+				<h1>References</h1>
+				<p>Hoffman, R. A. (1983). Grade inflation and student evaluations of college courses. Educational
+				and Psychological Research, 3, 51–160. https://doi.org/10.1023/A:101557981</p>
+				<p>Hoffman, R. A. (1983). Grade inflation and student evaluations of college courses. Educational
+				and Psychological Research, 3, 51–160. https://doi.org/10.1023/A:101557981</p>
+				<p>Hoffman, R. A. (1983). Grade inflation and student evaluations of college courses. Educational
+				and Psychological Research, 3, 51–160. https://doi.org/10.1023/A:101557981</p>
+				<p>Hoffman, R. A. (1983). Grade inflation and student evaluations of college courses. Educational
+				and Psychological Research, 3, 51–160. https://doi.org/10.1023/A:101557981</p>
+				<p>Hoffman, R. A. (1983). Grade inflation and student evaluations of college courses. Educational
+				and Psychological Research, 3, 51–160. https://doi.org/10.1023/A:101557981</p>
+				<p>Hoffman, R. A. (1983). Grade inflation and student evaluations of college courses. Educational
+				and Psychological Research, 3, 51–160. https://doi.org/10.1023/A:101557981</p>
+				<p>Hoffman, R. A. (1983). Grade inflation and student evaluations of college courses. Educational
+				and Psychological Research, 3, 51–160. https://doi.org/10.1023/A:101557981</p>
+				<p>Hoffman, R. A. (1983). Grade inflation and student evaluations of college courses. Educational
+				and Psychological Research, 3, 51–160. https://doi.org/10.1023/A:101557981</p>
+				<p>Hoffman, R. A. (1983). Grade inflation and student evaluations of college courses. Educational
+				and Psychological Research, 3, 51–160. https://doi.org/10.1023/A:101557981</p>
+			</div>
 		</div>
     )
 }
